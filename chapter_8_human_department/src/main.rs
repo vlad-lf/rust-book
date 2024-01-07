6647776fcc08bdc36ea81fe8facf0bdc7671ac21
vlad-lf <vladyslavkryukov@gmail.com>
2024-01-08 00:05:33 +0200

    8.3: implement add, list commands

--- chapter_8_human_department/src/main.rs
+++ chapter_8_human_department/src/main.rs
@@ -1,10 +1,12 @@
-use std::io;
+use std::{io, collections::HashMap};
 
 #[derive(Debug)]
 enum Command {
     Help,
     Exit,
-    NotImplemented
+    Add { name: String, department: String },
+    List,
+    Unknown
 }
 
 impl Command {
@@ -14,8 +16,13 @@ impl Command {
             Command::Help
         } else if command_line == "exit" {
             Command::Exit
+        } else if command_line == "list" {
+            Command::List
+        } else if command_line.starts_with("add") {
+            let add_command_split: Vec<&str> = command_line.split_whitespace().collect();
+            Command::Add { name: add_command_split[1].to_string(), department: add_command_split[3].to_string() }
         } else {
-            Command::NotImplemented
+            Command::Unknown
         }
     }
 }
@@ -23,6 +30,9 @@ impl Command {
 fn main() {
     println!("Hello and welcome to the Human Department management command-line interface!");
     println!("Please, input the command");
+
+    let mut database: HashMap<String, Vec<String>> = HashMap::new();
+
     loop {
         let mut input = String::new();
 
@@ -30,12 +40,25 @@ fn main() {
             .read_line(&mut input)
             .expect("Failed to read line");
 
-        let command = Command::parse_command(&input[..]);
+        let command = Command::parse_command(&input);
 
         match command {
+            Command::Help => println!("Use `exit` to exit the program\nUse `add X to Y` to add person `X` to department `Y`"),
+            Command::List => println!("The current DB state is: {:#?}", database),
+            Command::Add { name, department } => {
+                println!("Adding {name} to {department}");
+                match database.get_mut(&department) {
+                    Some(dep_people) => {
+                        dep_people.push(name);
+                        dep_people.sort();
+                    },
+                    None => {
+                        database.insert(department, vec![name]);
+                    }
+                };
+            },
             Command::Exit => break,
-            Command::Help => println!("Use `exit` to exit the program"),
-            Command::NotImplemented => println!("Unknown command, try again or try using 'help' command to see available options"),
+            Command::Unknown => println!("Unknown command, try again or try using 'help' command to see available options"),
         }
 
     }
