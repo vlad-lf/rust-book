38e70e91774d6c37dd3dcb3337ea8acc4f455f25
vlad-lf <vladyslavkryukov@gmail.com>
2024-01-07 23:36:53 +0200

    8.3: basic parsing and execution

--- chapter_8_human_department/src/main.rs
+++ chapter_8_human_department/src/main.rs
@@ -1,3 +1,42 @@
+use std::io;
+
+#[derive(Debug)]
+enum Command {
+    Help,
+    Exit,
+    NotImplemented
+}
+
+impl Command {
+    fn parse_command(command_line: &str) -> Command {
+        let command_line = command_line.trim().to_lowercase();
+        if command_line == "help" {
+            Command::Help
+        } else if command_line == "exit" {
+            Command::Exit
+        } else {
+            Command::NotImplemented
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello and welcome to the Human Department management command-line interface!");
+    println!("Please, input the command");
+    loop {
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let command = Command::parse_command(&input[..]);
+
+        match command {
+            Command::Exit => break,
+            Command::Help => println!("Use `exit` to exit the program"),
+            Command::NotImplemented => println!("Unknown command, try again or try using 'help' command to see available options"),
+        }
+
+    }
 }
