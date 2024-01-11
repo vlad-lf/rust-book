9f381aa1a24f4642bb0a500a0528207ec232c557
vlad-lf <vladyslavkryukov@gmail.com>
2024-01-11 23:01:30 +0200

    20: final version

--- chapter_20_web_server/src/lib.rs
+++ chapter_20_web_server/src/lib.rs
@@ -3,34 +3,14 @@ use std::{
     thread,
 };
 
-type Job = Box<dyn FnOnce() + Send + 'static>;
-
-struct Worker {
-    id: usize,
-    handle: thread::JoinHandle<()>
-}
-
-impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        Worker {
-            id,
-            handle: thread::spawn(move || loop {
-                let job = receiver.lock().unwrap().recv().unwrap();
-
-                println!("Worker {id} got a job; executing.");
-    
-                job();
-            })
-        }
-    }
-}
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>
+    sender: Option<mpsc::Sender<Job>>,
 }
 
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
 impl ThreadPool {
-    // --snip--
     /// Create a new ThreadPool.
     ///
     /// The size is the number of threads in the pool.
@@ -51,7 +31,10 @@ impl ThreadPool {
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers, sender }
+        ThreadPool {
+            workers,
+            sender: Some(sender),
+        }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -60,6 +43,50 @@ impl ThreadPool {
     {
         let job = Box::new(f);
 
-        self.sender.send(job).unwrap();
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv();
+
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
+
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id} disconnected; shutting down.");
+                    break;
+                }
+            }
+        });
+
+        Worker {
+            id,
+            thread: Some(thread),
+        }
     }
 }
